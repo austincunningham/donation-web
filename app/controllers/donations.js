a7803fbdccec6c873405c84cd02469ca2ce1b77f
austincunningham <austincunningham@hotmail.com>
2016-09-27 13:57:22 +0100

    Accounts and Donations Controllers + Routes

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -2,25 +2,24 @@
 
 exports.home = {
 
-  handler: (request, reply) => {
-    reply.view('main', { title: 'Welcome to Donations' });
+  handler: function (request, reply) {
+    reply.view('home', { title: 'Make a Donation' });
   },
 
 };
 
-exports.signup = {
+exports.report = {
 
-  handler: (request, reply) => {
-    reply.view('signup', { title: 'Sign up for Donations' });
+  handler: function (request, reply) {
+    reply.view('report', { title: 'Donations to Date', });
   },
 
 };
 
-exports.login = {
+exports.donate = {
 
-  handler: (request, reply) => {
-    reply.view('login', { title: 'Login to Donations' });
+  handler: function (request, reply) {
+    reply.redirect('/report');
   },
 
 };
-
--- routes.js
+++ routes.js
@@ -1,11 +1,17 @@
 const Donations = require('./app/controllers/donations');
 const Assets = require('./app/controllers/assets');
+const Accounts = require('./app/controllers/accounts');
 
 module.exports = [
 
-  { method: 'GET', path: '/', config: Donations.home },
-  { method: 'GET', path: '/signup', config: Donations.signup },
-  { method: 'GET', path: '/login', config: Donations.login },
+  { method: 'GET', path: '/', config: Accounts.main },
+  { method: 'GET', path: '/signup', config: Accounts.signup },
+  { method: 'GET', path: '/login', config: Accounts.login },
+  { method: 'POST', path: '/login', config: Accounts.authenticate },
+  { method: 'GET', path: '/logout', config: Accounts.logout },
+
+  { method: 'GET', path: '/home', config: Donations.home },
+  { method: 'GET', path: '/report', config: Donations.report },
   {
     method: 'GET',
     path: '/{param*}',
