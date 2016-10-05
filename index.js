86f573addd79ed3f37925bffa7a635893cd97aec
austincunningham <austincunningham@hotmail.com>
2016-10-05 11:24:36 +0100

    Connect to Mongo via Mongoose

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 4000 });
 
-server.bind({
+/*server.bind({
   //currentUser: {},
   users: {},
   donations: [],
-});
+});*/
+
+require('./app/models/db');
+
 server.register([require('inert'), require('vision'), require('hapi-auth-cookie')], err => {
 
   if (err) {
