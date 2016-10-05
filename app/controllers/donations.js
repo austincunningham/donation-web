bfd02babd50985ecf0159e21d20c239302f1f3a9
austincunningham <austincunningham@hotmail.com>
2016-10-05 12:36:01 +0100

    Introduce Donation Model

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -1,4 +1,6 @@
 'use strict';
+const Donation = require('../models/donation');
+
 
 exports.home = {
 
@@ -8,26 +10,31 @@ exports.home = {
 
 };
 
-exports.report = {
+exports.donate = {
 
   handler: function (request, reply) {
-    reply.view('report', {
-      title: 'Donations to Date',
-      donations: this.donations,
-      user: this.currentUser,
+    let data = request.payload;
+    const donation = new Donation(data);
+    donation.save().then(newDonation => {
+      reply.redirect('/report');
+    }).catch(err => {
+      reply.redirect('/');
     });
   },
 
 };
 
-exports.donate = {
+exports.report = {
 
   handler: function (request, reply) {
-    let data = request.payload;
-    var donorEmail = request.auth.credentials.loggedInUser;
-    data.donor = this.users[donorEmail];
-    this.donations.push(data);
-    reply.redirect('/report');
+    Donation.find({}).exec().then(allDonations => {
+      reply.view('report', {
+        title: 'Donations to Date',
+        donations: allDonations,
+      });
+    }).catch(err => {
+      reply.redirect('/');
+    });
   },
 
 };
---
+++ app/models/donation.js
@@ -0,0 +1,12 @@
+/**
+ * Created by austin on 05/10/2016.
+ */
+const mongoose = require('mongoose');
+
+const donationSchema = mongoose.Schema({
+  amount: Number,
+  method: String,
+});
+
+const Donation = mongoose.model('Donation', donationSchema);
+module.exports = Donation;
