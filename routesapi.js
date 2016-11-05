f0a81a6e5eb245ba58b2f8fb668ecdf887770f7e
austincunningham <austincunningham@hotmail.com>
2016-11-05 17:13:57 +0000

    added tests for the api endpoint for many and one candidate.

--- routesapi.js
+++ routesapi.js
@@ -5,4 +5,5 @@ const CandidatesApi = require('./app/api/candidatesapi');
 
 module.exports = [
   { method: 'GET', path: '/api/candidates', config: CandidatesApi.find },
+  { method: 'GET', path: '/api/candidates/{id}', config: CandidatesApi.findOne },
 ];
---
+++ test/candidateapitest.js
@@ -0,0 +1,52 @@
+/**
+ * Created by austin on 05/11/2016.
+ */
+'use strict';
+
+const assert = require('chai').assert;
+var request = require('sync-request');
+
+suite('Candidate API tests', function () {
+
+  /*test('get candidates', function () {
+    const url = 'http://localhost:4000/api/candidates';
+    var res = request('GET', url);
+    const candidates = JSON.parse(res.getBody('utf8'));
+    assert.equal(2, candidates.length);
+
+  });*/
+  test('get candidates', function () {
+
+    const url = 'http://localhost:4000/api/candidates';
+    var res = request('GET', url);
+    const candidates = JSON.parse(res.getBody('utf8'));
+
+    assert.equal(2, candidates.length);
+
+    assert.equal(candidates[0].firstName, 'Lisa');
+    assert.equal(candidates[0].lastName, 'Simpson');
+    assert.equal(candidates[0].office, 'President');
+
+    assert.equal(candidates[1].firstName, 'Donald');
+    assert.equal(candidates[1].lastName, 'Simpson');
+    assert.equal(candidates[1].office, 'President');
+
+  });
+
+  test('get one candidate', function () {
+
+    const allCandidatesUrl = 'http://localhost:4000/api/candidates';
+    var res = request('GET', allCandidatesUrl);
+    const candidates = JSON.parse(res.getBody('utf8'));
+
+    const oneCandidateUrl = allCandidatesUrl + '/' + candidates[0]._id;
+    res = request('GET', oneCandidateUrl);
+    const oneCandidate = JSON.parse(res.getBody('utf8'));
+
+    assert.equal(oneCandidate.firstName, 'Lisa');
+    assert.equal(oneCandidate.lastName, 'Simpson');
+    assert.equal(oneCandidate.office, 'President');
+
+  });
+
+});
