3e2f1c935c67168cf7633e0c5d3f8485fd3398fb
austincunningham <austincunningham@hotmail.com>
2016-11-05 16:02:56 +0000

    added candidate to the list view

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -1,12 +1,19 @@
 'use strict';
 const Donation = require('../models/donation');
 const User = require('../models/user');
-
+const Candidate = require('../models/candidate');
 
 exports.home = {
 
   handler: function (request, reply) {
-    reply.view('home', { title: 'Make a Donation' });
+    Candidate.find({}).then(candidates => {
+      reply.view('home', {
+        title: 'Make a Donation',
+        candidates: candidates,
+      });
+    }).catch(err => {
+      reply.redirect('/');
+    });
   },
 
 };
@@ -15,10 +22,17 @@ exports.donate = {
 
   handler: function (request, reply) {
     var userEmail = request.auth.credentials.loggedInUser;
+    let userId = null;
+    let donation = null;
     User.findOne({ email: userEmail }).then(user => {
       let data = request.payload;
-      const donation = new Donation(data);
-      donation.donor = user._id;
+      userId = user._id;
+      donation = new Donation(data);
+      const rawCandidate = request.payload.candidate.split(',');
+      return Candidate.findOne({ lastName: rawCandidate[0], firstName: rawCandidate[1] });
+    }).then(candidate => {
+      donation.donor = userId;
+      donation.candidate = candidate._id;
       return donation.save();
     }).then(newDonation => {
       reply.redirect('/report');
@@ -26,13 +40,12 @@ exports.donate = {
       reply.redirect('/');
     });
   },
-
 };
 
 exports.report = {
 
   handler: function (request, reply) {
-    Donation.find({}).populate('donor').then(allDonations => {
+    Donation.find({}).populate('donor').populate('candidate').then(allDonations => {
       reply.view('report', {
         title: 'Donations to Date',
         donations: allDonations,
